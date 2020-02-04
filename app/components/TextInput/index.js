f58340853f0d7b4ece2abe2c3a6398e646d21075
sterben04 <aravindunni00@gmail.com>
2020-02-04 10:13:45 +0530

    first commit

---
+++ app/components/TextInput/index.js
@@ -0,0 +1,4 @@
+import inputWithButton from './inputWithButton';
+import styles from './styles';
+
+export { inputWithButton, styles};
---
+++ app/components/TextInput/inputWithButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text, TouchableHighlight, TextInput} from 'react-native';
+import styles from './styles';
+
+const inputWithButton =() => null;
+
+export default inputWithButton;
